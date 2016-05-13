320a0d911dc618d88809c8a0f755729044856b19
Henrique <miyamotohk@gmail.com>
2016-05-13 13:10:41 -0300

    teste camera

---
+++ video_cap.cpp
@@ -0,0 +1,51 @@
+#include "video.hpp"
+#include "videoio.hpp"
+#include "videoio.hpp"
+#include "highgui.hpp"
+#include "opencv.hpp"
+#include <iostream>
+
+using namespace cv;
+using namespace std;
+
+int main(int argc, char* argv[])
+{
+    VideoCapture cap(1); // open the video camera no. 0
+
+    if (!cap.isOpened())  // if not success, exit program
+    {
+        cout << "Cannot open the video cam" << endl;
+        return -1;
+    }
+
+   double dWidth = cap.get(CV_CAP_PROP_FRAME_WIDTH); //get the width of frames of the video
+   double dHeight = cap.get(CV_CAP_PROP_FRAME_HEIGHT); //get the height of frames of the video
+
+    cout << "Frame size : " << dWidth << " x " << dHeight << endl;
+
+    namedWindow("MyVideo",CV_WINDOW_AUTOSIZE); //create a window called "MyVideo"
+
+    while (1)
+    {
+        Mat frame;
+
+        bool bSuccess = cap.read(frame); // read a new frame from video
+
+        if (!bSuccess) //if not success, break loop
+        {
+             cout << "Cannot read a frame from video stream" << endl;
+             break;
+        }
+
+        Mat tratado = frame; //+ Scalar(-75, -75, -75);
+        imshow("MyVideo", tratado); //show the frame in "MyVideo" window
+
+        if (waitKey(30) == 27) //wait for 'esc' key press for 30ms. If 'esc' key is pressed, break loop
+           {
+                cout << "esc key is pressed by user" << endl;
+                break; 
+           }
+    }
+    return 0;
+
+}
