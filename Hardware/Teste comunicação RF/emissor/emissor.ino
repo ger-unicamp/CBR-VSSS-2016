f0e457af90c52edfcfe472fae908dc22253282f2
miyamotohk <miyamotohk@gmail.com>
2017-05-28 16:04:26 -0300

    adicionados programas do emissor e receptor por RF e biblioteca RF24

---
+++ Hardware/Teste comunicação RF/emissor/emissor.ino
@@ -0,0 +1,37 @@
+ /*
+Este programa envia duas variáveis inteiras, via rádio usando um módulo nRF24L01
+
+Programa original disponibilizado por: 
+http://www.bajdi.com
+
+No nosso Aduino Nano, as portas (CE,CSN) sao (3,4); no nosso Arduino Uno emissor sao (9,10).
+As portas para comunicacao SPI sao definidas por padrao no Uno e no Nano como: MOSI 11, MISO 12, SCK 13.
+*/
+
+#define CE 3
+#define CSN 4
+
+#include <SPI.h>
+#include "nRF24L01.h"
+#include "RF24.h"
+
+int joystick[2];
+
+RF24 radio(CE,CSN);
+
+const uint64_t pipe = 0xA2E8F0F0E1LL;
+
+void setup(void)
+{ Serial.begin(9600);
+  radio.begin();
+  radio.openWritingPipe(pipe);
+}
+
+void loop(void)
+{
+  joystick[0] = 111;
+  joystick[1] = 222;
+  
+  radio.write( joystick, sizeof(joystick) );
+  
+}
