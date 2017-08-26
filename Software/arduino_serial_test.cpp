e366dfc8e53135cecd19433669d53fc7790f6fb5
Henrique Miyamoto <miyamotohk@gmail.com>
2017-08-25 23:50:16 -0300

    programa que escreve na serial atualizado

--- Software/arduino_serial_test.cpp
+++ Software/arduino_serial_test.cpp
@@ -1,11 +1,31 @@
+/*
+Programa para enviar string pela serial para o Arduino.
+*/
+
 #include <stdio.h>
 #include <unistd.h>
+#include <string.h>
+#include <cstdlib>
 
-int main()
+int main(int argc, char *argv[])
 {
-	int data[] = {10,5,13};  //Random data we want to send
+    //Conferencia de parametros
+	if(argc < 5){
+        printf("Numero de argumentos invalido.\n");
+        /* Parametros: [porta serial][numero do robo][potencia do motor][potencia do motor] */
+        return 1;
+    }
+    //Escreve endereco da porta serial
+    char porta[13] = "";
+    strcat(porta,"/dev/tty");
+    strcat(porta,argv[1]);
+
+    //Dados a serem escritos
+    int data[] = {atoi(argv[2]),atoi(argv[3]),atoi(argv[4])};  //Random data we want to send
+    
     FILE *file;
-    file = fopen("/dev/ttyACM1","w");  //Opening device file
+    file = fopen(porta,"w");
+
     int i = 0;
     for(i = 0 ; i < 3 ; i++)
     {
