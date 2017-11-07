0edc80cba3c9d3099ce4f2cf8f47fde130949413
Guilherme R C <guilherme.riciolic@gmail.com>
2017-11-07 18:02:41 -0200

    Parâmetros da câmera modificados

--- Software/Calibration/parametros.sh
+++ Software/Calibration/parametros.sh
@@ -1,2 +1,1 @@
-v4l2-ctl -d /dev/video1 -c saturation=100 -c white_balance_temperature_auto=0 -c white_balance_temperature=4500 -c contrast=5 -c sharpness=50 -c backlight_compensation=0 -c brightness=40
-
+v4l2-ctl -d /dev/video0 -c saturation=150 -c white_balance_temperature_auto=0 -c white_balance_temperature=5500 -c contrast=7 -c sharpness=50 -c backlight_compensation=0 -c brightness=40
